7d422d087126cd36067ec3d375160da94e1e3f80
VENUGOBAL <VENUGOBAL@LAPTOP-H4MMLA92>
2025-07-02 15:09:36 +0530

    first commit

---
+++ delete_demo_github/src/test/java/delete_demo/democlass.java
@@ -0,0 +1,11 @@
+package delete_demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class democlass {
+	@Test
+	public void clickOnElectronics() {
+		Reporter.log("Eelctronics page is displayed",true);
+}
+}
